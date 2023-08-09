5b096f6598604d97cc9b7c2e5b214dcfae11c36e
asifiqbal15 <asif.iqbal15@wipro.com>
2023-08-09 10:08:56 +0530

    auto delete 3

--- Middleware/logger.js
+++ Middleware/logger.js
@@ -13,8 +13,8 @@ const infoFilter = format((info, opts) => {
 
 const logDir = 'logs';
 const path=require("path")
-// const file_name=path.join(__dirname,logDir,`${Date.now()}_server.log`)
-const file_name="/public/logs/server.log"
+const file_name=path.join("/",logDir,`server.log`)
+// const file_name="/public/logs/server.log"
 
 
 
